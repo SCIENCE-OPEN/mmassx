a248f377eafec51b7575dd2d6f76a7d0846aa0bf
Lukas Drahnik <ldrahnik@gmail.com>
2025-05-12 21:08:04 +0200

    Fixed to python3

--- mspy/mod_calibration.py
+++ mspy/mod_calibration.py
@@ -102,7 +102,7 @@ def _leastSquaresFit(model, parameters, data, maxIterations=None, limit=1e-7):
         niter += 1
         
         delta = solveLinEq(alpha+l*numpy.diagonal(alpha)*id,-0.5*numpy.array(chi_sq[1]))
-        next_p = map(lambda a,b: a+b, p, delta)
+        next_p = list(map(lambda a,b: a+b, p, delta))
         
         next_chi_sq, next_alpha = _chiSquare(model, next_p, data)
         if next_chi_sq > chi_sq:
@@ -153,7 +153,7 @@ class _DerivVar:
         nvars = max(len(a), len(b))
         a = a + (nvars-len(a))*[0]
         b = b + (nvars-len(b))*[0]
-        return map(func, a, b)
+        return list(map(func, a, b))
     
     def __getitem__(self, item):
         if item == 0:
@@ -201,26 +201,27 @@ class _DerivVar:
     
     def __mul__(self, other):
         if isinstance(other, _DerivVar):
-            return _DerivVar(self.value * other.value, self._mapderiv(lambda a,b: a+b, map(lambda x,f=self.value:f*x, other.deriv), map(lambda x,f=other.value:f*x, self.deriv)))
+            return _DerivVar(self.value * other.value, self._mapderiv(lambda a,b: a+b, list(map(lambda x,f=self.value:f*x, other.deriv)), list(map(lambda x,f=other.value:f*x, self.deriv))))
         else:
-            return _DerivVar(self.value * other, map(lambda x,f=other:f*x, self.deriv))
+            return _DerivVar(self.value * other, list(map(lambda x,f=other:f*x, self.deriv)))
     
-    def __div__(self, other):
+    def __truediv__(self, other):
         if isinstance(other, _DerivVar):
             inv = 1./other.value
-            return _DerivVar(self.value * inv, self._mapderiv(lambda a,b: a-b, map(lambda x,f=inv: f*x, self.deriv), map(lambda x,f=self.value*inv*inv: f*x, other.deriv)))
+            return _DerivVar(self.value * inv, list(self._mapderiv(lambda a,b: a-b, list(map(lambda x,f=inv: f*x, self.deriv)), list(map(lambda x,f=self.value*inv*inv: f*x, other.deriv)))))
         else:
-            inv = 1./value
-            return _DerivVar(self.value * inv, map(lambda x,f=inv:f*x, self.deriv))
+            inv = 1./other
+            return _DerivVar(self.value * inv, list(map(lambda x,f=inv:f*x, self.deriv)))
     
     def __pow__(self, other):
         val1 = pow(self.value, other-1)
-        deriv1 = map(lambda x,f=val1*other: f*x, self.deriv)
+        deriv1 = list(map(lambda x,f=val1*other: f*x, self.deriv))
         return _DerivVar(val1*self.value, deriv1)
         
     def __abs__(self):
         absvalue = abs(self.value)
-        return _DerivVar(absvalue, map(lambda a, d=self.value/absvalue: d*a, self.deriv))
+        return _DerivVar(absvalue, list(map(lambda a, d=self.value/absvalue: d*a, self.deriv)))
+
     
 # ----
 
